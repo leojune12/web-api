7c9288445dc344aedf961140467e09b5bc535156
Leo <leojunebedeo@gmail.com>
2023-08-20 15:07:10 +0800

    Add route file

--- app/public/index.php
+++ app/public/index.php
@@ -1,21 +1,5 @@
 <?php
 
-use Classes\Controllers\Api\UserController;
-
 require __DIR__ . "/../bootstrap/bootstrap.php";
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
-
-if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
-    header("HTTP/1.1 404 Not Found");
-    exit();
-}
-
-require __DIR__ . "/../Classes/Controllers/Api/UserController.php";
-$objFeedController = new UserController();
-$strMethodName = $uri[3] . 'Action';
-$objFeedController->{$strMethodName}();
-
-echo "index";
-?>
+require_once __DIR__ . "/../routes/web.php";
---
+++ app/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Classes\Controllers\Api\UserController;
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+require __DIR__ . "/../Classes/Controllers/Api/UserController.php";
+$objFeedController = new UserController();
+$strMethodName = $uri[3] . 'Action';
+$objFeedController->{$strMethodName}();
