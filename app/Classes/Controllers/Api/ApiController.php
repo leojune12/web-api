b689a586180977a558b68cec9bf5c6a825a95919
Leo <leojunebedeo@gmail.com>
2023-08-23 00:50:06 +0800

    Remove unused methods

--- app/Classes/Controllers/Api/ApiController.php
+++ app/Classes/Controllers/Api/ApiController.php
@@ -11,26 +11,4 @@ class ApiController
     {
         $this->sendOutput('', array('HTTP/1.1 404 Not Found'));
     }
-
-    /** 
-    * Get URI elements. 
-    * 
-    * @return array 
-    */
-    protected function getUriSegments()
-    {
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $uri = explode( '/', $uri );
-        return $uri;
-    }
-
-    /** 
-    * Get querystring params. 
-    * 
-    * @return array 
-    */
-    protected function getQueryStringParams()
-    {
-        return parse_str($_SERVER['QUERY_STRING'], $query);
-    }
 }
