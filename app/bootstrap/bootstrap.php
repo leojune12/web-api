137742a32c2d6b2b34c7cc60b61a6bf6628b93b6
Leo <leojunebedeo@gmail.com>
2023-08-22 13:54:38 +0800

    Add classes autoloader

--- app/bootstrap/bootstrap.php
+++ app/bootstrap/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
+
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");
-// include main configuration file 
+
 require_once PROJECT_ROOT_PATH . "/bootstrap/config.php";
-// include the base controller file 
-require_once PROJECT_ROOT_PATH . "/Classes/Controllers/Api/Controller.php";
-// include the use model file 
-require_once PROJECT_ROOT_PATH . "/Classes/Models/UserModel.php";
-?>
+
+require_once "autoloader.php";
